0df12085f5a9c1509c907cc070fcb78a207f3ea8
NoHeal4u <miodragnikolic@yahoo.com>
2018-04-19 21:40:48 +0200

    uradjen i osmi

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Team;
 use \App\Comment;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\CommentReceived;
 
 class CommentsController extends Controller
 {   
@@ -14,9 +16,10 @@ class CommentsController extends Controller
         $this->middleware('hateMiddleware',['only' => 'store']);
     }
 
-    public function store()
+    public function store($team_id)
     {
-    	
+    	$team = Team::find($team_id);
+        
 
     	$comment = new Comment;
     	$this->validate(request(),['content'=>'required|min:10']);
@@ -26,6 +29,8 @@ class CommentsController extends Controller
         $comment->team_id = request('team_id');
         
         $comment->save();
+
+        Mail::to($team)->send(new CommentReceived($team));
 
     	return redirect()->back();
 
---
+++ resources/views/email/comment-received.blade.php
@@ -0,0 +1,2 @@
+
+<p>Dear {{ $team->name}} you have a new comment on your team page. </p>
